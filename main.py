43e8351c2ced795dec80df501c18c832d89252b1
maidazuniga <mzuniga.g@uc.cl>
2025-12-03 10:47:20 -0300

    refactor: codigo modular

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
 import os
 import smtplib
 from datetime import datetime
-import time
 
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.chrome.webdriver import WebDriver
+
+import validar_empresa
 
 load_dotenv()
 
@@ -41,11 +37,7 @@ class BotValidaciones:
 
         fecha = datetime.now().strftime("%d/%m/%Y %H:%M")
 
-        if self.hay_errores:
-            asunto = f"Reporte Diario ERP - {fecha}"
-        else:
-            asunto = f"Reporte Diario ERP - {fecha}"
-
+        asunto = f"Reporte de Validaciones ERP - {fecha}"
         cuerpo_mensaje = "Resumen de la ejecución:\n\n" + "\n".join(self.log_eventos)
         
         msg = MIMEMultipart()
@@ -55,7 +47,7 @@ class BotValidaciones:
         msg.attach(MIMEText(cuerpo_mensaje, 'plain'))
 
         try:
-            print("\n Conectando con servidor de correo...")
+            print("\nConectando con servidor de correo...")
             server = smtplib.SMTP('smtp.gmail.com', 587)
             server.starttls()
             server.login(origen, clave)
@@ -67,52 +59,20 @@ class BotValidaciones:
 
 def ejecutar_validacion():
     bot = BotValidaciones()
-    usuario = os.getenv('USARIO')
-    clave = os.getenv('CLAVE')
-    url = os.getenv('URL')
-
     driver = webdriver.Chrome()
     driver.maximize_window()
-    driver.get(url)
-    wait = WebDriverWait[WebDriver](driver, 10)
-
+    
     try:
-        campo_usuario = wait.until(EC.presence_of_element_located((By.ID, "nombreusuario")))
-        campo_clave = driver.find_element(By.ID, "claveusuario")
-        boton_entrar = driver.find_element(By.ID, "EntrarBtn") 
-
-        print("Escribiendo credenciales...")
-        campo_usuario.clear()
-        campo_usuario.send_keys(usuario)
-        
-        campo_clave.clear()
-        campo_clave.send_keys(clave)
-
-        boton_entrar.click()
-        
-        bot.registrar_mensaje("Verificando que aparezca la empresa")
-        
-        try:
-            validar_presencia_empresa = wait.until(EC.presence_of_element_located((By.ID, "EmpresaDDL")))
-            dropdown = Select(validar_presencia_empresa)
-            empresas_disponibles = len(dropdown.options)
-
-            if empresas_disponibles > 0:
-                bot.registrar_mensaje(f"Validación exitosa")
-            else:
-                bot.registrar_mensaje(f"ERROR CRÍTICO: El selector de empresas está vacío", es_error=True)
-        
-            time.sleep(5) 
-
-        except Exception as e:
-            bot.registrar_mensaje(f"Error técnico buscando el selector: {str(e)}", es_error=True)
+        validar_empresa.validar(driver, bot)
 
     except Exception as e:
         bot.registrar_mensaje(f"Error general en el bot: {str(e)}", es_error=True)
     
     finally:
+        print("Finalizando ejecución y cerrando navegador...")
         driver.quit()
         bot.enviar_reporte_correo()
 
 if __name__ == "__main__":
     ejecutar_validacion()
+
---
+++ validar_empresa.py
@@ -0,0 +1,50 @@
+import os
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+
+def validar(driver, bot):
+    usuario = os.getenv('USARIO')
+    clave = os.getenv('CLAVE')
+    url = os.getenv('URL')
+
+    driver.get(url)
+    wait = WebDriverWait(driver, 10)
+
+    try:
+        campo_usuario = wait.until(EC.presence_of_element_located((By.ID, "nombreusuario")))
+        campo_clave = driver.find_element(By.ID, "claveusuario")
+        boton_entrar = driver.find_element(By.ID, "EntrarBtn") 
+
+        bot.registrar_mensaje("Iniciando login...")
+        campo_usuario.clear()
+        campo_usuario.send_keys(usuario)
+        
+        campo_clave.clear()
+        campo_clave.send_keys(clave)
+
+        boton_entrar.click()
+        
+        bot.registrar_mensaje("Verificando que aparezca la empresa...")
+        
+        try:
+            validar_presencia_empresa = wait.until(EC.presence_of_element_located((By.ID, "EmpresaDDL")))
+            dropdown = Select(validar_presencia_empresa)
+            empresas_disponibles = len(dropdown.options)
+
+            if empresas_disponibles > 0:
+                bot.registrar_mensaje(f"Verificación de empresa exitosa.")
+            else:
+                bot.registrar_mensaje(f"ERROR CRÍTICO: El selector de empresas está vacío.", es_error=True)
+        
+            time.sleep(5) 
+
+        except Exception as e:
+            bot.registrar_mensaje(f"Error técnico buscando el selector: {str(e)}", es_error=True)
+    
+    except Exception as e:
+        bot.registrar_mensaje(f"Error en el proceso de login: {str(e)}", es_error=True)
+
