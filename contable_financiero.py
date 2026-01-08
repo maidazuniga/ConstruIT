74ec1bf0f385aaf56e0c04db52bff68c1a43aa4a
maidazuniga <mzuniga.g@uc.cl>
2026-01-08 15:30:56 -0300

    feat: registrar factura

---
+++ contable_financiero.py
@@ -0,0 +1,44 @@
+from typing import Any
+import time
+import os
+import random
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+def registro_factura(driver, bot, num_orden):
+    wait = WebDriverWait(driver, 10)
+    bot.registrar_mensaje(f"Validando registro de factura {num_orden}...")
+
+    try:
+        btn_factura = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[href*='Contable/DocumentoContable']")))
+        btn_factura.click()
+
+        orden = wait.until(EC.element_to_be_clickable((By.ID, "ctl00_phContenidoCentral_OrdenesCompraTxt"))) 
+        orden.send_keys(num_orden)   
+        cargar_datos = wait.until(EC.presence_of_element_located((By.ID, "ctl00_phContenidoCentral_Label18")))
+        cargar_datos.click()
+        time.sleep(1.5)
+
+        num_documento = wait.until(EC.presence_of_element_located((By.ID, "ctl00_phContenidoCentral_NroDocumentoTxt")))
+        num_documento.send_keys(str(random.randint(1000, 9999)))
+        time.sleep(0.5)
+
+        btn_grabar = wait.until(EC.element_to_be_clickable((By.ID, "ctl00_Label2")))
+        btn_grabar.click()
+
+        lbl_mensaje = wait.until(EC.visibility_of_element_located((By.ID, "ctl00_phContenidoCentral_MensajeLbl")))
+        enlace_numero = lbl_mensaje.find_element(By.TAG_NAME, "a")
+        num_factura = enlace_numero.text.strip()
+
+        bot.registrar_mensaje("Validación exitosa.\n")
+
+        driver.get(os.getenv('URL_BASE'))
+        time.sleep(2)
+        return num_factura
+
+    except Exception as e:
+        bot.registrar_error(e, "Módulo de Contable/Registro de Factura")
+        pass
